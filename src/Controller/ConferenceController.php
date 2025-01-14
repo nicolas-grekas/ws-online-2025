f1b1929ca1a6f31c13a6d63f47529dc10b06b710
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-01-14 12:36:52 +0100

    add entities

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ConferenceController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(
+        #[MapQueryParameter()]
+        string $hello = 'World',
+    ): Response
+    {
+        $greet = '';
+        if ($hello) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($hello));
+        }
+
+        return new Response(<<<EOF
+            <html>
+                <body>
+                    $greet
+                    <img src="/images/under-construction.gif" />
+                </body>
+            </html>
+            EOF
+        );
+    }
+}

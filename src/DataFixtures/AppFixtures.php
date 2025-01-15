2a69883a55cf482c7ccb217052c39284ce037d3a
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-01-15 12:03:44 +0100

    functional test 1

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Admin;
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(
+        private PasswordHasherFactoryInterface $passwordHasherFactory,
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $conference = new Conference();
+        $conference->setCity('Paris');
+        $conference->setYear('2019');
+        $conference->setIsInternational(false);
+        $manager->persist($conference);
+
+        $comment1 = new Comment();
+        $comment1->setConference($conference);
+        $comment1->setAuthor('Fabien');
+        $comment1->setEmail('fabien@example.com');
+        $comment1->setText('This was a great conference.');
+        $manager->persist($comment1);
+
+        $conference = new Conference();
+        $conference->setCity('Madrid');
+        $conference->setYear('2020');
+        $conference->setIsInternational(true);
+        $manager->persist($conference);
+
+        $admin = new Admin();
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setUsername('admin');
+        $admin->setPassword($this->passwordHasherFactory->getPasswordHasher(Admin::class)->hash('admin'));
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
